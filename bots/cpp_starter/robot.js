1cfb3fcdab433e09e1981fda259c8450b6ac74a7
Daniel Keyes <danieljkeyes@gmail.com>
2019-01-13 16:49:32 -0800

    initialize the bindings during the first turn, to avoid 1st turn timeout bug

--- bots/cpp_starter/robot.js
+++ bots/cpp_starter/robot.js
@@ -3,15 +3,15 @@ import {BCAbstractRobot, SPECS} from 'battlecode';
 import Module from './wasm_lib_GENERATED.js'
 
 let bindings = null;
-Module().then(function (Module) {
-    bindings = Module;
-});
 
 let nativeRobot = null;
 
 class MyRobot extends BCAbstractRobot {
     turn() {
         if (!nativeRobot) {
+            Module().then(function (Module) {
+                bindings = Module;
+            });
             nativeRobot = bindings.AbstractNativeRobot.createNativeRobotImpl(this);
         }
         return nativeRobot.turn();
