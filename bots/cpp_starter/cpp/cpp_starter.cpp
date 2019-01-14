38cbcff0adcfcecdfcc4dabdb3d028019c20aa8f
Daniel Keyes <danieljkeyes@gmail.com>
2019-01-13 16:00:09 -0800

    reformatting
    We should see about adding a clang-format. That's a little more consistent
    than clion.

--- bots/cpp_starter/cpp/cpp_starter.cpp
+++ bots/cpp_starter/cpp/cpp_starter.cpp
@@ -1,4 +1,3 @@
-
 #include <emscripten/bind.h>
 
 #include "cpp_starter.h"
@@ -9,8 +8,7 @@ using emscripten::val;
 
 // Binding code
 EMSCRIPTEN_BINDINGS(my_robot_example) { // NOLINT
-  class_<AbstractNativeRobot>("AbstractNativeRobot")
-      .class_function<std::unique_ptr<AbstractNativeRobot> >("createNativeRobotImpl",
-                                                             &AbstractNativeRobot::createNativeRobotImpl)
-      .function("turn", &AbstractNativeRobot::turn);
+  class_<AbstractNativeRobot>("AbstractNativeRobot").class_function<std::unique_ptr<AbstractNativeRobot> >(
+      "createNativeRobotImpl",
+      &AbstractNativeRobot::createNativeRobotImpl).function("turn", &AbstractNativeRobot::turn);
 }
